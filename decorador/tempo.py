242c5e0b6e1187620c9a8429ae856268bfa89ead
Rafael Henrique <oraffaudi@gmail.com>
2020-04-21 13:05:00 -0300

    Novos arquivos

--- decorador/tempo.py
+++ decorador/tempo.py
@@ -4,9 +4,9 @@
 #
 # def logar(f):
 #     @wraps(f)
-#     def executar_com_tempo(*arg, **kargs):
+#     def executar_com_tempo(*args, **kwargs):
 #         print(strftime('%H:%M:%S'))
-#         return f(*arg, **kargs)
+#         return f(*args, **kwargs)
 #
 #     return  executar_com_tempo
 #
@@ -28,29 +28,66 @@
 #     print(ola('Rafael'))
 
 
+# from functools import wraps
+# from time import sleep, strftime
+#
+#
+# def logar(fn=None, *, fmt='%H:%M:%S'):
+#     if fn is not None:
+#         return logar(fmt=fmt)(fn)
+#
+#     def decorator(f):
+#         @wraps(f)
+#         def executar_com_tempo(*args, **kwargs):
+#             print(strftime(fmt))
+#             return f(*args, **kwargs)
+#
+#         return executar_com_tempo
+#
+#     return decorator
+#
+#
+# @logar
+# def mochileiro():
+#     return 42
+#
+#
+# @logar(fmt='%H:%M:%S')
+# def ola(nome):
+#     return f'Olá {nome}'
+#
+#
+# if __name__ == '__main__':
+#     print(mochileiro())
+#     print(mochileiro.__name__)
+#     print(ola('Renzo'))
+#     print(ola.__name__)
+#     sleep(1)
+#     print((mochileiro()))
+#     print(ola('Rafael'))
+
 from functools import wraps
 from time import sleep, strftime
+from decorator import getfullargspec, decorator
 
-def logar(fmt='%H:%M:%S'):
-    def decorator(f):
-        @wraps(f)
-        def executar_com_tempo(*arg, **kargs):
-            print(strftime(fmt))
-            return f(*arg, **kargs)
+@decorator
+def logar(f, fmt='%H:%M:%S', *args, **kwargs):
+    print(strftime(fmt))
+    return f(*args, **kwargs)
 
-        return  executar_com_tempo
-
-    return decorator
-
-@logar(fmt='%H:%M:%S')
+@logar
 def mochileiro():
     return 42
+
 
 @logar(fmt='%H:%M:%S')
 def ola(nome):
     return f'Olá {nome}'
 
+
 if __name__ == '__main__':
+    print(getfullargspec(mochileiro))
+    print(getfullargspec(ola))
     print(mochileiro())
     print(mochileiro.__name__)
     print(ola('Renzo'))
